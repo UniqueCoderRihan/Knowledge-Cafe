a1d657df768a79e65002dcac0212ebf8cfe5a046
Rihanul Islam <rihanthedev@gmail.com>
2023-04-01 02:37:45 +0600

    spenTime added

--- src/Components/SpentTime/SpenTime.js
+++ src/Components/SpentTime/SpenTime.js
@@ -6,7 +6,7 @@ const SpenTime = ({spentime}) => {
 
     return (
         <div className='time border-2 border-blue-500 rounded-lg'>
-            <h2 className='text-2xl text-center font-semibold'>Total Spen Time: {local}</h2>
+            <h2 className='text-2xl text-center font-semibold'>Total Spen:{local} Min</h2>
         </div>
     );
 };
